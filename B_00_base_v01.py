34636edb4edb9b64a8f51ea79603b879b6e75698
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2024-02-23 12:28:01 +1300

    Pick random raffle winner. added to base code

--- B_00_base_v01.py
+++ B_00_base_v01.py
@@ -1,5 +1,6 @@
 from time import sleep
 import pandas
+import random
 # Functions go here
 
 
@@ -98,7 +99,7 @@ def instructions():
 
 
 # Heading
-statement_generator("Conversion Calculator for Weight, Distance & Time", "-")
+statement_generator("Mini Movie Fundraiser", "-")
 
 # Set maximum number of tickets
 MAX_TICKETS = 5
@@ -168,7 +169,7 @@ while tickets_sold < MAX_TICKETS:
 
 # Create data frame from dictionary to organise information
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
-mini_movie_frame = mini_movie_frame.set_index('Name')
+# mini_movie_frame = mini_movie_frame.set_index('Name')
 
 # Calculate the total ticket cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
@@ -186,18 +187,31 @@ add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
 for var_item in add_dollars:
     mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
 
-print("---- Ticket Data ----")
+# Choose a winner from our name list
+winner_name = random.choice(all_names)
+
+# Get position of winner name in list
+win_index = all_names.index(winner_name)
+
+# Look up total amount won (ie: ticket price + surcharge)
+total_won = mini_movie_frame.at[win_index, 'Total']
+
+statement_generator("Ticket Data", "-")
 print()
 
 # Output table with ticket data
 print(mini_movie_frame)
 
 print()
-print("----- Ticket Cost / Profit -----")
+statement_generator("Ticket Cost / Profit", "-")
 
 # Output total ticket sales and profit
 print("Total Ticket Sales: ${:.2f}".format(total))
 print("Total Profit: ${:.2f}".format(profit))
+
+print()
+statement_generator("Raffle Winner", "*")
+print("Congratulations {}. You have won {} ie: your ticket is free!".format(winner_name, total_won))
 
 # Output number of tickets sold
 if tickets_sold == MAX_TICKETS:
